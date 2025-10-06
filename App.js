51c7f7db1c6b69203f6d56523e3820545675f588
Rafael Sá <rnsa@minsait.com>
2025-10-06 15:58:54 -0300

    corrige bug de importação

--- App.js
+++ App.js
@@ -1,16 +1,19 @@
-import { NavigationContainer } from "@react-navigation/native";
-import { StatusBar } from "react-native";
-import AuthProvider from "./app/context/auth";
-import Routes from "./app/routes";
+import 'react-native-gesture-handler';
+import React from 'react';
+import { StatusBar } from 'react-native';
 
-export default function App() {
-  return (
+import { NavigationContainer } from '@react-navigation/native';
+
+import Routes from './app/routes';
+import AuthProvider from './app/context/auth';
+
+export default function App(){
+  return(
     <NavigationContainer>
       <AuthProvider>
-        <StatusBar backgroundColor="#f0f4ff" barStyle="dark-content" />
+        <StatusBar backgroundColor="#F0F4FF" barStyle="dark-content" />
         <Routes/>
       </AuthProvider>
     </NavigationContainer>
-  );
+  )
 }
-
--- app/context/auth.js
+++ app/context/auth.js
@@ -1,38 +1,77 @@
-import { createContext, useState } from "react";
+import React, { createContext, useState } from 'react';
 
-import { useNavigation } from "@react-navigation/native";
-import api from '../services/api'
+import api from '../services/api';
+import { useNavigation } from '@react-navigation/native';
 
-const AuthContext = createContext({});
+export const AuthContext = createContext({});
 
-function AuthProvider ({children}){
-    const [user, setUser] = useState(null)
-    const [loadingAuth, setLoadingAuth] = useState(false);
+function AuthProvider({ children }){
+  const [user, setUser] = useState(null); 
+  const [loadingAuth, setLoadingAuth] = useState(false);
 
-    const navigation = useNavigation();
+  const navigation = useNavigation();
 
-    async function signUp(email, password, nome){
-        setLoadingAuth(true);
-        try {
-            await api.post('/users', {
-                name: nome,
-                password: password,
-                email: email,
-            })
-            
-            setLoadingAuth(false);
-            navigation.goBack();
-        } catch (err) {
-            console.log("ERRO AO CADASTRAR", err);
-            setLoadingAuth(false);
-        }
+
+  async function signUp(email, password, nome){
+    setLoadingAuth(true);
+
+    try{
+      const response = await api.post('/users', {
+       name: nome,
+       password: password,
+       email: email,
+      })
+      setLoadingAuth(false);
+
+      navigation.goBack();
+
+
+    }catch(err){
+      console.log("ERRO AO CADASTRAR", err);
+      setLoadingAuth(false);
+    }
+  }
+
+  async function signIn(email, password){
+    setLoadingAuth(true);
+
+    try{
+      const response = await api.post('/login', {
+        email: email,
+        password: password
+      })
+
+      const { id, name, token } = response.data;
+
+      const data = {
+        id,
+        name,
+        token,
+        email,
+      };
+
+      api.defaults.headers['Authorization'] = `Bearer ${token}`;
+
+      setUser({
+        id,
+        name,
+        email,
+      })
+
+      setLoadingAuth(false);
+
+    }catch(err){
+      console.log("ERRO AO LOGAR ", err);
+      setLoadingAuth(false);
     }
 
-    return (
-        <AuthContext.Provider value={{ signed: !!user,  user, signUp, loadingAuth}}>
-            {children}
-        </AuthContext.Provider>
-    )
+  }
+
+  return(
+    <AuthContext.Provider value={{ signed: !!user, user, signUp, signIn, loadingAuth }}>
+      {children}
+    </AuthContext.Provider>
+  )
 }
 
 export default AuthProvider;
